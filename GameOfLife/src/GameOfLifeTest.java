a351075570d912470f097d7c4ec0daf7320c771d
ericm <ericmlowry456@gmail.com>
2020-11-09 15:39:52 -0500

    Currently functional.

---
+++ GameOfLife/src/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameOfLifeTest {
+
+	@Test
+	void test_NewStateGrid() {
+		boolean[][] grid = {{true}};
+		GameOfLife One = new GameOfLife(grid);
+		
+	}
+
+}
